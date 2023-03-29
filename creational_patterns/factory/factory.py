fb772721966ef539c2478bd8db7bf1b909fc659d
narin <narin.tent.95@gmail.com>
2023-03-29 09:50:10 +0700

    factory pattern

---
+++ creational_patterns/factory/factory.py
@@ -0,0 +1,40 @@
+from rich import inspect as insp
+from termcolor import colored
+
+class Animal():
+    body = "Four leg"
+
+    def speak(self):
+        pass
+
+    def display(self):
+        return self.body
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meaow"
+
+class Bird(Animal):
+    body= "2 Leg have wing can fly"
+
+class AnimalFactory:
+    def get_animal(self,animaltype=None):
+        match animaltype:
+            case 'cat':
+                return Cat()
+            case 'dog':
+                return Dog()
+            case 'bird':
+                return Bird()
+
+factory = AnimalFactory()
+cat = factory.get_animal('cat')
+bird = factory.get_animal('bird')
+dog = factory.get_animal('dog')
+print(cat.speak())
+print(dog.speak())
+print(bird.display())
